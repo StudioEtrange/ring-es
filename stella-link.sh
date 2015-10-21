789ebaecc25c3c8c6c2c226e8bd5faf49bf2d18b
StudioEtrange <nomorgan@gmail.com>
2015-10-21 11:41:02 +0200

    merge

--- stella-link.sh
+++ stella-link.sh
@@ -2,7 +2,7 @@
 _STELLA_LINK_CURRENT_FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export STELLA_ROOT=$_STELLA_LINK_CURRENT_FILE_DIR/../stella
 STELLA_DEP_FLAVOUR=DEV
-STELLA_DEP_VERSION=0.0.2-17-g2460a6e
+STELLA_DEP_VERSION=0.0.2-73-g1f90cc9
 [ ! "$1" == "chaining" ] && STELLA_APP_ROOT=$_STELLA_LINK_CURRENT_FILE_DIR
 
 if [ ! "$1" == "nothing" ]; then
