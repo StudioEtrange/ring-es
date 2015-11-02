16e2a9a6fc42bad81d0e2e7334eff1b3c0a4efc4
StudioEtrange <nomorgan@gmail.com>
2015-11-02 15:55:40 +0100

    update stella link

--- stella-link.sh
+++ stella-link.sh
@@ -8,9 +8,9 @@ STELLA_DEP_VERSION=0.0.2-73-g1f90cc9
 if [ ! "$1" == "nothing" ]; then
 	if [ ! "$1" == "bootstrap" ]; then
 		if [ ! -f "$STELLA_ROOT/stella.sh" ]; then
-			if [ -f "$(dirname $STELLA_ROOT)/stella-link.sh" ]; then
-				echo " ** Try to chain link stella from $(dirname $STELLA_ROOT)"
-				source $(dirname $STELLA_ROOT)/stella-link.sh chaining
+			if [ -f "$(dirname $_STELLA_LINK_CURRENT_FILE_DIR)/stella-link.sh" ]; then
+				echo " ** Try to chain link stella from $(dirname $_STELLA_LINK_CURRENT_FILE_DIR)"
+				source $(dirname $_STELLA_LINK_CURRENT_FILE_DIR)/stella-link.sh chaining
 			else
 				echo "** WARNING Stella is missing -- bootstraping stella"
 				$_STELLA_LINK_CURRENT_FILE_DIR/stella-link.sh bootstrap
